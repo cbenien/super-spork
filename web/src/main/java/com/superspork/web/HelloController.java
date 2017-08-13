7f5cacb03c39b28f3aba50ab09f1fb75217d4f06
Christian Benien <christian.benien@lighting.com>
2017-08-13 20:14:34 +0200

    Added name method

--- web/src/main/java/com/superspork/web/HelloController.java
+++ web/src/main/java/com/superspork/web/HelloController.java
@@ -4,21 +4,42 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 @Controller
 public class HelloController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    static int counter = 0;
+    private static ConcurrentMap<String,Integer> counters = new ConcurrentHashMap<String, Integer>();
 
     @GetMapping("/")
     public String index(Model model) {
-        counter ++;
-        logger.info("Main action invoked, current counter is {}", counter);
+        String defaultName = "Harry Potter";
+        int currentCount = IncrementByName(defaultName);
+        logger.info("Main action invoked, current default counter is {}", currentCount);
         model.addAttribute("name", "Harry Potter");
-        model.addAttribute("counter", counter);
+        model.addAttribute("counter", currentCount);
         return "home";
     }
+
+    @GetMapping("/name/{name}")
+    public String name(@PathVariable("name") String name, Model model) {
+        int currentCount = IncrementByName(name);
+        logger.info("Main action invoked, current counter for {} is {}", name, currentCount);
+        model.addAttribute("name", name);
+        model.addAttribute("counter", currentCount);
+        return "home";
+    }
+
+    private static int IncrementByName(String name)
+    {
+        counters.putIfAbsent(name, 0);
+        return counters.compute(name, (key, value) -> value + 1);
+    }
+
 
 }
 
--- web/src/test/java/com/superspork/web/WebApplicationTests.java
+++ web/src/test/java/com/superspork/web/WebApplicationTests.java
@@ -2,15 +2,63 @@ package com.superspork.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class WebApplicationTests {
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
 	@Test
-	public void contextLoads() {
+	public void indexAction_works() {
+
+        String body = this.restTemplate.getForObject("/", String.class);
+        System.out.println(body);
+
+        assertThat(body).contains("Harry Potter");
+        assertThat(body).contains("1 times");
+
 	}
+
+    @Test
+    public void nameAction_works() {
+
+	    String name = "gamma";
+
+        String body = this.restTemplate.getForObject("/name/" + name, String.class);
+        System.out.println(body);
+
+        assertThat(body).contains(name);
+        assertThat(body).contains("1 times");
+
+    }
+
+    @Test
+    public void nameAction_worksWithMultipleNames() {
+
+        String name1 = "alpha";
+        String name2 = "beta";
+
+        String body1 = null, body2 = null;
+
+        for (int i=0; i<3; i++)
+            body1 = this.restTemplate.getForObject("/name/" + name1, String.class);
+
+        for (int i=0; i<10; i++)
+            body2 = this.restTemplate.getForObject("/name/" + name2, String.class);
+
+        assertThat(body1).contains(name1);
+        assertThat(body2).contains(name2);
+
+        assertThat(body1).contains("3 times");
+        assertThat(body2).contains("10 times");
+
+    }
 
 }
